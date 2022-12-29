dfbc9aeb7e2948709e7501eb3328049509ceee3a
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-12-30 00:32:55 +0200

    refactor home page, created a banner section component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,39 +5,15 @@ import {
   CardContent,
   CardMedia,
   Container,
-  CssBaseline,
   TextField,
   Typography,
 } from '@mui/material';
+import BannerSection from '../../components/sections/BannerSection';
 
 const Home = () => {
   return (
     <>
-      <Container
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          height: '400px',
-        }}
-      >
-        <CssBaseline />
-        <Box sx={{ width: '50%', m: 1 }}>
-          <Card sx={{ width: '100%' }}>
-            <CardMedia
-              sx={{ height: '400px', width: '100%', objectFit: 'contain' }}
-              image="https://images.pexels.com/photos/4005033/pexels-photo-4005033.jpeg?auto=compress&cs=tinysrgb&w=800"
-            />
-          </Card>
-        </Box>
-        <Box sx={{ width: '50%', m: 1 }}>
-          <Card sx={{ width: '100%' }}>
-            <CardMedia
-              sx={{ height: '400px', width: '100%' }}
-              image="https://images.pexels.com/photos/5868722/pexels-photo-5868722.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-          </Card>
-        </Box>
-      </Container>
+      <BannerSection />
       <Container
         sx={{
           display: 'flex',
