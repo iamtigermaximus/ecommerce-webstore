9cd87766b4870e21257d87e80be0d4be4142b938
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-12-30 01:17:28 +0200

    refactor homepage, created newsletter section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, Container, TextField, Typography } from '@mui/material';
 import BannerSection from '../../components/sections/BannerSection';
 import CategorySection from '../../components/sections/CategorySection';
 import MostPopularSection from '../../components/sections/MostPopularSection';
 import NewArrivalsSection from '../../components/sections/NewArrivalsSection';
+import NewsletterSection from '../../components/sections/NewsletterSection';
 
 const Home = () => {
   return (
@@ -11,76 +11,7 @@ const Home = () => {
       <CategorySection />
       <NewArrivalsSection />
       <MostPopularSection />
-      <Container
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          height: '100%',
-          width: '100vw',
-          marginTop: '50px',
-        }}
-      >
-        <Box
-          sx={{
-            bgcolor: 'rgb(243, 243, 243)',
-            width: '60%',
-            display: 'flex',
-            flexDirection: 'column',
-            p: 4,
-          }}
-        >
-          <Typography
-            variant="h5"
-            gutterBottom
-            sx={{
-              fontWeight: 700,
-            }}
-          >
-            Subscribe to our Newsletter
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            Subscribe now and get 10% off of one order. Be among the first to
-            hear about benefits, offers and events.
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            bgcolor: 'rgb(243, 243, 243)',
-            width: '40%',
-            display: 'flex',
-            flexDirection: 'column',
-            p: 4,
-          }}
-        >
-          <Box
-            sx={{
-              width: 500,
-              maxWidth: '100%',
-            }}
-          >
-            <TextField fullWidth label="Email" />
-          </Box>
-          <Box
-            sx={{
-              py: 2,
-            }}
-          >
-            <Button
-              variant="contained"
-              sx={{
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                bgcolor: 'orange',
-                color: 'black',
-              }}
-            >
-              Subscribe
-            </Button>
-          </Box>
-        </Box>
-      </Container>
+      <NewsletterSection />
     </>
   );
 };
